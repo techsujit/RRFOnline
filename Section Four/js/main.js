34516ac00f905031ac0418f9c4d6a295895259a8
Sujit Barua <cse.sujitbarua@gmail.com>
2017-07-28 04:56:55 +0600

    Section Four
    Secction Four

---
+++ Section Four/js/main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+
+    $(".team-carousel").owlCarousel({
+        items:3,
+        loop:true,
+        margin:30,
+        nav:true,
+        navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+
+    });
+
+    });
+
+
+
+
